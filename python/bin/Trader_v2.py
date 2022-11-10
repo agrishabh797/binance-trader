1798bb5d27c3bb1b0ae285d4f65f8ada6a3af496
agrawalrishabh <rishabh.agrawal@pattern.com>
2022-11-10 11:41:26 +0530

    Changes to the scripts

--- python/bin/Trader_v2.py
+++ python/bin/Trader_v2.py
@@ -661,7 +661,7 @@ def create_new_positions(max_positions, conn, um_futures_client):
     sql_sell = "select coalesce(count(current_margin), 0) from positions where position_status = 'OPEN' and side = 'SELL'"
 
     # update
-    total_positions = 10
+    total_positions = 6
     cursor = conn.cursor()
 
     cursor.execute(sql_buy)
