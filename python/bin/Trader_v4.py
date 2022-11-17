b3818f0e818ff393e83ada96b20e707468285b96
agrawalrishabh <rishabh.agrawal@pattern.com>
2022-11-17 21:19:24 +0530

    Changes to the scripts

--- python/bin/Trader_v4.py
+++ python/bin/Trader_v4.py
@@ -63,8 +63,8 @@ def create_take_profit_order(symbol, position_id, current_margin, side, conn, um
     position_quantity = abs(float(response[0]['positionAmt']))
     total_position_amount = entry_price * position_quantity
 
-    # 25% of margin is our profit
-    profit = float((25 * current_margin) / 100)
+    # 22% of margin is our profit
+    profit = float((22 * current_margin) / 100)
 
     if side == 'BUY':
         profit_position_amount = total_position_amount + profit
