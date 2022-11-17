56ca14650efcf85ec77a7f2bc36d76ede7e171b4
agrawalrishabh <rishabh.agrawal@pattern.com>
2022-11-17 22:14:46 +0530

    Changes to the scripts

--- python/bin/Trader_v4.py
+++ python/bin/Trader_v4.py
@@ -25,8 +25,8 @@ def create_stop_loss_order(symbol, position_id, current_margin, side, conn, um_f
     position_quantity = abs(float(response[0]['positionAmt']))
     total_position_amount = entry_price * position_quantity
 
-    # 10% of margin is our loss
-    profit = float((10 * current_margin) / 100)
+    # 8% of margin is our loss
+    profit = float((8 * current_margin) / 100)
 
     if side == 'BUY':
         loss_position_amount = total_position_amount - profit
@@ -63,8 +63,8 @@ def create_take_profit_order(symbol, position_id, current_margin, side, conn, um
     position_quantity = abs(float(response[0]['positionAmt']))
     total_position_amount = entry_price * position_quantity
 
-    # 22% of margin is our profit
-    profit = float((22 * current_margin) / 100)
+    # 20% of margin is our profit
+    profit = float((20 * current_margin) / 100)
 
     if side == 'BUY':
         profit_position_amount = total_position_amount + profit
