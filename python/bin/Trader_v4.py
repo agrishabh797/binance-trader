b94b2faa8bbbbf467975100a90702b93341f638d
agrawalrishabh <rishabh.agrawal@pattern.com>
2022-11-28 01:09:20 +0530

    Changes to the scripts

--- python/bin/Trader_v4.py
+++ python/bin/Trader_v4.py
@@ -522,7 +522,7 @@ def get_rounded_quantity(symbol, price, um_futures_client):
 def create_position(symbol, side, each_position_amount, conn, um_futures_client):
 
 
-    leverage = 20
+    leverage = 10
     exchange_info = get_exchange_info(symbol, um_futures_client)
     current_time = datetime.utcnow()
     current_timestamp = current_time.strftime('%Y-%m-%d %H:%M:%S')
