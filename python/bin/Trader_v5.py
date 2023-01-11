fdb7c8201b33f522798b3583413d7143e78aca9d
agrawalrishabh <rishabh.agrawal@pattern.com>
2023-01-11 13:16:53 +0530

    Changes to the scripts

--- python/bin/Trader_v5.py
+++ python/bin/Trader_v5.py
@@ -752,7 +752,7 @@ def create_new_positions(max_positions, conn, um_futures_client):
     leverage = 20
 
     total_new_positions = new_buy_pos_count + new_sell_pos_count
-    if open_pos_count <= 4:
+    if open_pos_count <= 2:
         logging.info("Last batch completed, creating new batch of %s positions", str(total_positions))
         new_positions_symbols = get_new_positions_symbols(total_new_positions, new_buy_pos_count, new_sell_pos_count, conn, um_futures_client)
         total_wallet_amount = get_total_wallet_amount(conn, um_futures_client)
